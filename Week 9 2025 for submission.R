16906d53e596907c0b58d682f63240f2649e6818
AntonBachMoeller <117591018+AntonBachMoeller@users.noreply.github.com>
2025-02-28 11:26:57 +0100

    Add files via upload

---
+++ Week 9 2025 for submission.R
@@ -0,0 +1,15 @@
+rooms <- c(1,5,2,1,3,1,NA,3,1,3,2,1,NA,1,8,3,1,4,NA,1,3,1,2,1,7,1,NA)
+rooms2 <- c(rooms>2)
+
+#rooms2 counts the instances of "TRUE" in rooms
+sum(rooms2, na.rm = TRUE)
+
+str(rooms)
+
+median(rooms, na.rm = TRUE)
+
+library(tidyverse)
+read_csv("data/SAFI_clean.csv")
+interviews <- read_csv("data/Safi_clean.csv")
+head(interviews)
+interviews
